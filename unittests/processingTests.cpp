7142c42278cfbcfaf05e0ba24928870a57516f39
lerandc <lerandc@umich.edu>
2020-07-02 15:38:29 -0700

    create test suite for post-processing and analysis

---
+++ unittests/processingTests.cpp
@@ -0,0 +1,21 @@
+#include <boost/test/unit_test.hpp>
+#include "ArrayND.h"
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <random>
+#include "utility.h"
+#include "fileIO.h"
+
+namespace Prismatic{
+
+BOOST_AUTO_TEST_SUITE(processingTests);
+
+BOOST_AUTO_TEST_CASE(gaussians)
+{
+    BOOST_TEST(1==0);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+} //namespace Prismatic
